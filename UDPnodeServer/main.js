df773fce2436156acee04d46a3ac603e47b9280e
Emil Christiansen <emil.christiansen02@pm.me>
2026-08-13 23:21:33 +0200

    speedometer and middleman server

--- UDPnodeServer/main.js
+++ UDPnodeServer/main.js
@@ -1,19 +1,52 @@
-const dgram = require('dgram');
+import express from 'express'
+import { createServer } from "http"
+import { WebSocketServer } from "ws"
 
-const server = dgram.createSocket('udp4');
-const client = dgram.createSocket('udp4');
+import dgram from 'dgram'
 
-server.on('message', (msg) => {
+const app = express();
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
+const port = 3001
+
+wss.broadcast = (msg) => {
+  console.log(msg)
+  wss.clients.forEach(function each(client)  {
+    client.send(msg)
+  })
+}
+
+const UDPserver = dgram.createSocket('udp4');
+
+UDPserver.on('message', (msg) => {
   console.log('UDP string: ' + msg + '\n')
+  wss.broadcast(msg)
 })
 
-server.bind(1234, () => {
+UDPserver.bind(1234, () => {
 
   // Getting the reference of server
   // by using ref() method
-  const size = server.ref();
+  const size = UDPserver.ref();
 
   // display the result
   console.log(size.eventNames());
 
 });
+
+wss.on("connection", (ws) => {
+  console.log("New client connected")
+
+  ws.on("message", (data) => {
+    const messageText = data.toString()
+    console.log(`Recieved: ${messageText}`)
+  })
+
+  ws.on("close", () => {
+    console.log("Client disconnected")
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
