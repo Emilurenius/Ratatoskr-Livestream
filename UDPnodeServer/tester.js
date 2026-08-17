d85844fb2c5cde81ca94c968df5d3da832b19bfb
Emil Christiansen <emilsen68@gmail.com>
2026-08-17 10:16:44 +0200

    Connected to groundstation

--- UDPnodeServer/tester.js
+++ UDPnodeServer/tester.js
@@ -4,7 +4,7 @@ const client = dgram.createSocket('udp4');
 
 // Client sending message to server
 // by using send() method
-client.send("v:0001", 0, 6, 1234, "localhost", (err) => {
+client.send("v:0222", 0, 6, 1234, "localhost", (err) => {
 
   if (err) throw err;
   console.log("message sent");
